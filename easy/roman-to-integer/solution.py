d19df19ccad460fd47716231da2725217bd99974
Spador <sp3466@rit.edu>
2025-10-28 21:57:00 -0400

    Add solution implementation for Roman to Integer (13)

--- easy/roman-to-integer/solution.py
+++ easy/roman-to-integer/solution.py
@@ -1,23 +1,25 @@
 class Solution:
-    ROMAN = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    
     def romanToInt(self, s: str) -> int:
-        val = 0
-        prev = 0
+        # Map Roman symbols to their integer values
+        romanMap = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
         
-        for c in s:
-            curr = self.ROMAN[c]
-            if prev < curr:
-                val -= prev + prev
-            val += curr
-            prev = curr
-            
-        return val 
+        result = 0
+        
+        # Process each character in the string
+        for i in range(len(s)):
+            # If current value is less than next value, subtract it
+            # Otherwise, add it to the result
+            if (i + 1 < len(s)) and romanMap[s[i]] < romanMap[s[i + 1]]:
+                result -= romanMap[s[i]]
+            else:
+                result += romanMap[s[i]]
+        
+        return result
