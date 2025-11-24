4aec26e7e0713168f406ecbd550b73c4a490508b
Spador <sp3466@rit.edu>
2025-11-24 13:40:00 -0500

    Add solution for Coin Change

---
+++ medium/coin-change/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+# spador
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Bottom-up recursive DP solution
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], (1 + dp[i - c]))
+        return dp[amount] if dp[amount] != amount + 1 else -1
