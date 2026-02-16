cd1baadb40a2514efbfa3cefb137b0016540ec60
Spador <sp3466@rit.edu>
2026-02-16 14:15:00 -0500

    Add solution for Symmetric Tree

---
+++ easy/symmetric-tree/solution.py
@@ -0,0 +1,42 @@
+# spador
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        # recursive solution
+        def dfs(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+
+            return ((left.val == right.val) and
+                   dfs(left.left, right.right) and
+                   dfs(left.right, right.left))
+        return dfs(root.left, root.right)
+
+        # iterative solution
+
+        queue = collections.deque([root, root])
+        while queue:
+            t1 = queue.pop()
+            t2 = queue.pop()
+
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+
+        return True
