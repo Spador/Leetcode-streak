fc818ae9027ef65f97cab70c8f2e14655446b66b
Spador <sp3466@rit.edu>
2026-02-21 14:07:00 -0500

    Add solution for Missing Number

---
+++ easy/missing-number/solution.py
@@ -0,0 +1,11 @@
+# spador
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = (n * (n+1)) // 2
+
+        for i in nums:
+            total -= i
+
+        return total
