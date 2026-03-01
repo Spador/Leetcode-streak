37b9353c94a2b8c4704b3feb8f19da876d0187dc
Spador <sp3466@rit.edu>
2026-03-01 16:03:00 -0500

    Add solution for Single Number

---
+++ easy/single-number/solution.py
@@ -0,0 +1,9 @@
+# spador
+
+# use the XOR operation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for n in nums:
+            result = n ^ result
+        return result
