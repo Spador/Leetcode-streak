8cf0f2e102182a25febdedac6526a2ca7705bfd4
Spador <sp3466@rit.edu>
2026-03-01 16:26:00 -0500

    Add solution for Number of 1 Bits

--- easy/number-of-1-bits/solution.py
+++ easy/number-of-1-bits/solution.py
@@ -1,9 +1,20 @@
+#spador
+
+##Solution 1: shift right and mod by 2. If remainder is 1, increment the result otherwise continue. Do this until all 32 bits are 0.
+
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
         result = 0
-        #Solution 1: shift right and mod by 2. If remainder is 1, increment the result otherwise continue. Do this until all 32 bits are 0.
+        # Solution 1
 
         while n:
             result += n%2
             n = n >> 1
-        return result 
+        return result
+
+        # Solution 2
+        while n:
+            n = n & (n - 1)
+            result += 1
+        return result
