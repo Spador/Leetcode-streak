6c2bc32943f2cba7c9087044f65dba0727db2b43
Spador <sp3466@rit.edu>
2026-03-23 11:06:00 -0400

    Add solution for Plus One

---
+++ easy/plus-one/solution.py
@@ -0,0 +1,13 @@
+# spador
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+            # if all digits are 9 like 9, 99, 999....
+            if i == 0:
+                return [1] + digits
