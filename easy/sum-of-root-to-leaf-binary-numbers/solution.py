419f3e5e8e5d09827dd183d67337dac2d357d322
Spador <sp3466@rit.edu>
2026-03-31 23:15:41 -0400

    Add solution for Sum of Root To Leaf Binary Numbers

---
+++ easy/sum-of-root-to-leaf-binary-numbers/solution.py
@@ -0,0 +1,23 @@
+# spador
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def dfs(node, curr) -> int:
+            if not node:
+                return 0
+            
+            curr = curr * 2 + node.val
+            if not node.left and not node.right:
+                return curr
+            
+            return dfs(node.left, curr) + dfs(node.right, curr)
+        
+        return dfs(root, 0)
