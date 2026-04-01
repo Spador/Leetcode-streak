083001e203b5b4f8c8f142cad2be0824468643b6
Spador <sp3466@rit.edu>
2026-03-31 23:19:33 -0400

    Add solution for Binary Tree Inorder Traversal

---
+++ easy/binary-tree-inorder-traversal/solution.py
@@ -0,0 +1,33 @@
+# spador
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        # recursive solution
+        def dfs(node):
+            if not node:
+                return []
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+            return result
+        # return dfs(root)
+
+        # iterative solution
+        curr, stack = root, []
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+        
+        return result
