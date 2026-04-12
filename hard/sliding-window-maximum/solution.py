99502d59cce988165ffb1d46057434c07866cac5
Spador <sp3466@rit.edu>
2026-04-12 15:12:00 -0400

    Add Sliding Window Maximum (239) - Solution

---
+++ hard/sliding-window-maximum/solution.py
@@ -0,0 +1,22 @@
+# spador
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = collections.deque()
+        result = []
+        left = right = 0
+
+        while right < len(nums):
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
+
+            if left > queue[0]:
+                queue.popleft()
+
+            if right - left + 1 >= k:
+                result.append(nums[queue[0]])
+                left += 1
+            right += 1
+
+        return result
