7c644bbf975ec967abbf01f315b3be0c00613409
Spador <sp3466@rit.edu>
2026-04-18 15:04:00 -0400

    Add solution for 252. Meeting Rooms

---
+++ easy/meeting-rooms/solution.py
@@ -0,0 +1,13 @@
+# spador
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals: return True
+        intervals.sort()
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start < prevEnd:
+                return False
+            else:
+                prevEnd = end
+        return True
