9aa7143ad0a9384157da60fcc908f4b902f9f320
Spador <sp3466@rit.edu>
2026-04-18 14:11:00 -0400

    Add solution2 for 115. Distinct Subsequences

---
+++ hard/distinct-subsequences/solution2.py
@@ -0,0 +1,20 @@
+# spador
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+
+        for c in range(len(t) + 1):
+            dp[len(s)][c] = 0
+
+        for r in range(len(s) + 1):
+            dp[r][len(t)] = 1
+
+
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(len(t) - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j]
+        return dp[0][0]
