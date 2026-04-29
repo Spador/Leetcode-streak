bfdf3f6b6018648e667c9fdf42be14443baa8701
Spador <sp3466@rit.edu>
2026-04-29 11:27:00 -0400

    Add solution for 708. Insert into a Sorted Circular Linked List

---
+++ medium/insert-into-a-sorted-circular-linked-list/solution.py
@@ -0,0 +1,29 @@
+##Spador
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        if not head:
+            node = Node(insertVal)
+            node.next = node
+            return node
+
+        curr = head
+        while curr.next != head:
+            if curr.val <= insertVal <= curr.next.val:
+                break
+            if curr.val > curr.next.val and (curr.val <= insertVal or insertVal <= curr.next.val):
+                break
+            curr = curr.next
+
+        node = Node(insertVal, curr.next)
+        curr.next = node
+
+        return head
